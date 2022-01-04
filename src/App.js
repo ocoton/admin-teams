48f7c200871912b471cb2fa09aeb711d1dbfdb97
oliver coton <oliver.coton@gmail.com>
2022-01-04 16:48:19 +0100

    Rendering of read and create componants for User and Team within the root level off the App.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import User from "./User";
+import CreateUsers from './CreateUsers';
+import ReadUser from './ReadUser';
+import CreateTeams from './CreateTeams';
+import ReadTeams from './ReadTeams';
+import { Button } from "@blueprintjs/core";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  return( 
+    // <Router>   
+      <div>
+        <div className="App">
+          <h1> User CRUD operations </h1>
+          {/* <User /> */}
+        </div>
+        <button type="submit" href=""> 
+          Create New
+        </button>
+        <button type="submit" href=""> 
+          Delete All
+        </button>
+        <CreateUsers />
+        <ReadUser />
+        <CreateTeams />
+        <ReadTeams />
+        <Button intent="success" text="button content" />
 
-          
-        </a>
-        
-      </header>
-      <User />
-    </div>
+        {/* <Routes> */}
+          {/* <Route exact path='/createuser' component={CreateUsers} /> */}
+          {/* <Route exact path='/readuser' component={ReadUser} /> */}
+        {/* </Routes> */}
+      </div>
+    // </Router>
   );
 }
 
